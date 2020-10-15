9cda69c2850b328232627bb22e2d8ccb0e488c5f
Thieu Mao <thieumao@gmail.com>
2020-10-15 23:24:29 +0700

    Add more images

--- lib/main.dart
+++ lib/main.dart
@@ -30,8 +30,15 @@ void main() async {
     'flies/macho-fly-1.png',
     'flies/macho-fly-2.png',
     'flies/macho-fly-dead.png',
+    'bg/lose-splash.png',
+    'branding/title.png',
+    'ui/dialog-credits.png',
+    'ui/dialog-help.png',
+    'ui/icon-credits.png',
+    'ui/icon-help.png',
+    'ui/start-button.png',
   ]);
-  
+
   LangawGame game = LangawGame();
   runApp(game.widget);
   // BoxGame game = BoxGame();
